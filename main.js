f946bea1cd8a36cc1f6a5dd4ee97052fe1eaf0f7
Jieun Kim <ikeun08054@naver.com>
2024-07-10 18:26:19 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+
+if (addButton) {
+    addButton.addEventListener("click", addTask);
+}
+
+function addTask() {
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    };
+    taskList.push(task);
+    console.log(taskList);
+    render();
+}
+
+function render() {
+    let resultHTML = "";
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+                <div class="task-done">${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                </div>
+            </div>`;
+        } else {
+            resultHTML += `<div class="task">
+                <div>${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                </div>
+            </div>`;
+        }
+    }
+    document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function deleteTask(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
